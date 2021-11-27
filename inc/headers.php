0364cde231500ae1758448ab7c754080024dfa71
RoniJii <81521266+RoniJii@users.noreply.github.com>
2021-11-27 15:58:48 +0200

    login päivitys

--- inc/headers.php
+++ inc/headers.php
@@ -1,4 +1,12 @@
 <?php
+
+include('../fb/BeforeValidException.php');
+include('../fb/ExpiredException.php');
+include('../fb/JWK.php');
+include('../fb/JWT.php');
+include('../fb/Key.php');
+include('../fb/SignatureInvalidException.php'); 
+
 if (isset($_SERVER['HTTP_ORIGIN'])) {
 header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
 header('Access-Control-Allow-Credentials:true');
